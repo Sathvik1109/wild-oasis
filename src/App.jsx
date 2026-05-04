234faa593abd5b2d64c7c8019d3e42305e0a5c2e
Sathvik1109 <saathvikdugyala09@gmail.com>
2026-05-04 17:08:38 -0500

    building the header

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,29 @@
+import styled from "styled-components";
 import GlobalStyle from "./styles/GlobalStyle";
 import Button from "./ui/Button";
 import Input from "./ui/Input";
+import Heading from "./ui/Heading";
+
+const StyledApp = styled.div`
+  background-color: aquamarine;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
 
 const App = () => {
   return (
     <>
       <GlobalStyle />
-      <Button onClick={() => alert("Hi bey")}>Click me</Button>
-      <Input type="text" placeholder="Guess the number" />
+      <StyledApp>
+        <Heading as="h1">The wild Oasis</Heading>
+        <Heading as="h2">Check in & out</Heading>
+        <Heading as="h3">Welcome the Oasis</Heading>
+        <Button onClick={() => alert("Hi bey")}>Click me</Button>
+        <Input type="text" placeholder="Guess the number" />
+      </StyledApp>
     </>
   );
 };
