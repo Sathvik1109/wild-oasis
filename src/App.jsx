0644e7aba8fbe44cb80b1c55214947d2b70b7da4
Sathvik1109 <saathvikdugyala09@gmail.com>
2026-05-04 16:32:21 -0500

    exported styling into components

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,14 @@
-import styled from "styled-components";
-
-const H1 = styled.h1`
-  font-size: 50px;
-  font-weight: 600;
-  background-color: aliceblue;
-`;
-
-const Button = styled.button`
-  font-size: 1.4rem;
-  padding: 1.2rem 1.6rem;
-  font-weight: 500;
-  border: none;
-  border-radius: 7px;
-  background-color: aliceblue;
-  cursor: pointer;
-`;
-
-const Input = styled.input`
-  border: none;
-  border-radius: 20px;
-  background-color: antiquewhite;
-  color: black;
-  padding: 0.5rem 0 0.5rem 2rem;
-  width: 30rem;
-  /* font-size: 1.2rem; */
-  font-size: 1rem;
-  font-weight: lighter;
-  margin: 2rem 0;
-`;
-
-const StyledApp = styled.div`
-  background-color: aquamarine;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  height: 100vh;
-`;
+import GlobalStyle from "./styles/GlobalStyle";
+import Button from "./ui/Button";
+import Input from "./ui/Input";
 
 const App = () => {
   return (
-    <StyledApp>
-      <H1>The wild Oasis</H1>
+    <>
+      <GlobalStyle />
       <Button onClick={() => alert("Hi bey")}>Click me</Button>
       <Input type="text" placeholder="Guess the number" />
-    </StyledApp>
+    </>
   );
 };
 
---
+++ src/ui/Input.jsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+const Input = styled.input`
+  border: none;
+  border-radius: 20px;
+  background-color: antiquewhite;
+  color: black;
+  padding: 0.5rem 0 0.5rem 2rem;
+  width: 30rem;
+  /* font-size: 1.2rem; */
+  font-size: 1rem;
+  font-weight: lighter;
+  margin: 2rem 0;
+`;
+
+export default Input;
