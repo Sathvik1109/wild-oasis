339762b93a67fb85e59b7c6857c7bc21cebb1f32
Sathvik1109 <saathvikdugyala09@gmail.com>
2026-05-13 02:23:02 -0500

    feat: adding new cabin input to the supabase db

--- src/features/cabins/CreateCabinForm.jsx
+++ src/features/cabins/CreateCabinForm.jsx
@@ -6,6 +6,9 @@ import Button from "../../ui/Button";
 import FileInput from "../../ui/FileInput";
 import Textarea from "../../ui/Textarea";
 import { useForm } from "react-hook-form";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { createCabin } from "../../services/apiCabins";
+import toast from "react-hot-toast";
 
 const FormRow = styled.div`
   display: grid;
@@ -44,9 +47,26 @@ const Error = styled.span`
 `;
 
 function CreateCabinForm() {
-  const { register, handleSubmit } = useForm();
+  const { register, handleSubmit, reset } = useForm();
+  const queryClient = useQueryClient();
 
-  const onSubmit = (data) => console.log(data);
+  const { mutate, isPending } = useMutation({
+    mutationFn: createCabin,
+    onSuccess: () => {
+      toast.success("New cabin successfully created");
+      queryClient.invalidateQueries({
+        queryKey: ["cabin"],
+      });
+    },
+    onError: (err) => {
+      toast.error(err.message);
+    },
+  });
+
+  const onSubmit = (data) => {
+    mutate(data);
+    reset();
+  };
 
   return (
     <Form onSubmit={handleSubmit(onSubmit)}>
@@ -95,7 +115,7 @@ function CreateCabinForm() {
         <Button variation="secondary" type="reset">
           Cancel
         </Button>
-        <Button>Edit cabin</Button>
+        <Button disabled={isPending}>Add cabin</Button>
       </FormRow>
     </Form>
   );
--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -1,5 +1,6 @@
 import supabase from "./supabase";
 
+// Fetching Cabin Data
 export const getCabins = async () => {
   const { data, error } = await supabase.from("cabins").select("*");
 
@@ -11,12 +12,28 @@ export const getCabins = async () => {
   return data;
 };
 
+// Deleting Cabin Data
 export const deleteCabin = async (id) => {
   const { data, error } = await supabase.from("cabins").delete().eq("id", id);
 
   if (error) {
     console.error(error);
     throw new Error("Cabins could not be loaded");
+  }
+
+  return data;
+};
+
+// Creating Cabin Data
+export const createCabin = async (newCabin) => {
+  const { data, error } = await supabase
+    .from("cabins")
+    .insert([newCabin])
+    .select();
+
+  if (error) {
+    console.error(error);
+    throw new Error("Cabins could not be created");
   }
 
   return data;
